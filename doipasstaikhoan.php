304c883024d93a1b4ffab1dc18799a157e47942f
dinhtai98 <54591175+dinhtai98@users.noreply.github.com>
2019-09-04 15:51:27 +0700

    Delete doipasstaikhoan.php

--- doipasstaikhoan.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "connect.php";
-    $tendn = $_POST['tendn'];
-    $passmoi = $_POST['password'];
-    $query = "UPDATE admin SET password='$passmoi' WHERE username = '$tendn'";
-    $data = mysqli_query($conn, $query);
-?>
